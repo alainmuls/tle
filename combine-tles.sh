53046f7952ca36103fdccb1212db27826fec044f
alainmuls <alain.muls@gmail.com>
2021-05-10 13:08:00 +0200

    adding TLEs

---
+++ combine-tles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# combine the individual NORAD TLEs in the cmb directory
+DIRS=`find . -maxdepth 1 -type d -name '20*' | sort`
+NORADIDS=`find . -maxdepth 1 -type f -name '*-NORAD-only.t'`
+
+# echo 'DIRS = '${DIRS}
+# echo 'NORADIDS = '${NORADIDS}
+
+for noradid in `cat ${NORADIDS}`
+do
+	echo ${noradid}
+	norad_files=''
+	for dir in ${DIRS}
+	do
+		# echo sat${noradid}.txt
+		norad_files+=`find ${dir} -name sat${noradid}.txt`' '
+		# echo 'norad files are: '${norad_files}
+	done
+	cat ${norad_files} > cmb/sat${noradid}.txt
+done
