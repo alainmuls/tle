ab37d09a40abd4ed67173260e2d434c287a78490
alainmuls <alain.muls@gmail.com>
2021-05-12 14:53:56 +0200

    trying to add G14 and G18

--- combine-tles.sh
+++ combine-tles.sh
@@ -2,12 +2,26 @@
 
 # combine the individual NORAD TLEs in the cmb directory
 DIRS=`find . -maxdepth 1 -type d -name '20*' | sort`
-NORADIDS=`find . -maxdepth 1 -type f -name '*-NORAD-only.t'`
+NORADIDS=`find . -maxdepth 1 -type f -name '*-NORAD-PRN.t'`
 
-# echo 'DIRS = '${DIRS}
-# echo 'NORADIDS = '${NORADIDS}
+echo 'DIRS = '${DIRS}
+echo 'NORADIDS = '${NORADIDS}
 
-for noradid in `cat ${NORADIDS}`
+echo -n > /tmp/NORAD-all.t
+for noradf in ${NORADIDS}
+do
+	cat ${noradf} >> /tmp/NORAD-all.t
+done
+
+# extract column with NORAD IDs and get uniq list
+awk -F "," '{print $4}' /tmp/NORAD-all.t | sort | uniq > /tmp/NORAD-IDs.t
+sed -i 's/.$//'  /tmp/NORAD-IDs.t
+
+wc -l /tmp/NORAD-all.t
+wc -l /tmp/NORAD-IDs.t
+exit 2
+
+for noradid in `cat ${NORADIDS} | sort`
 do
 	echo ${noradid}
 	norad_files=''
